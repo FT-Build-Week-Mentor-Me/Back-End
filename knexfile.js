732bc4913a0287458a07761bf60e67eae2108a9f
Tony <tonyrkovar@gmail.com>
2019-11-19 14:20:04 -0600

    set up and wrote tests

--- knexfile.js
+++ knexfile.js
@@ -19,6 +19,25 @@ module.exports = {
         },
     },
 
+    testing: {
+        client: "sqlite3",
+        useNullAsDefault: true,
+        connection: {
+            filename: "./data/test.db3"
+        },
+        migrations: {
+            directory: './data/migrations'
+        },
+        seeds: {
+            directory: './data/seeds'
+        },
+        pool: {
+            afterCreate: (connection, done) => {
+                connection.run("PRAGMA foreign_keys = ON", done)
+            }
+        }
+    },
+
     staging: {
         client: 'postgresql',
         connection: {
---
+++ routers/router-models.spec.js
@@ -0,0 +1,57 @@
+const request = require('supertest');
+const server = require('../auth/server')
+
+const db = require('../data/db-config')
+
+
+const users = require('../models/users-model')
+const threads = require('../models/threads-model')
+const comments = require('../models/comments-model')
+
+describe('Testing the users models', () => {
+    describe('get a single user', () => {
+        it('should return matching  user names', async () => {
+            const result = await users.findById(2);
+            expect(result.username).toBe('bigBuisnessBoi')
+        })
+    })
+    //commenting out the below test: It works but you have to manually change the username and email for every test
+
+    // describe('testing the register method', () => {
+    //     it('should return status 200', async () => {
+    //         const register = await users.register({
+    //             username: "JestTestUser3",
+    //             password: "TESTUSERJEST",
+    //             email: "jestertester3@email.com",
+    //             profile_type: "mentor"
+
+    //         })
+    //         expect(register.username).toBe("JestTestUser2")
+    //     })
+    // })
+})
+
+describe('testing the comments model', () => {
+    describe('GET comments', () => {
+        it('Should return a single comment', async () => {
+            const result = await request(server)
+                .get(`/api/comments/${3}`)
+            expect(result.body).not.toHaveLength(0)
+        })
+        it("should return a list of all comments", async () => {
+            const comments = await request(server)
+                .get('/api/comments')
+            expect(comments.body).not.toHaveLength(0)
+            expect(comments.status).toBe(200)
+        })
+    })
+})
+
+describe('testing the threads endpoints/model', () => {
+    describe('testing threads gets', () => {
+        it('will return a list of threads', async () => {
+            const allThreads = await threads.findAllThreads()
+            expect(allThreads[2].id).toBe(3)
+        })
+    })
+})
